387979ecaf5a99082eeea062eafc94bfaaa0e53f
bambo1543 <andreas.bga@gmail.com>
2025-02-25 20:53:32 +0100

    fixed PositionPnL.java

--- src/main/java/com/bybit/api/client/domain/position/response/positionData/PositionPnL.java
+++ src/main/java/com/bybit/api/client/domain/position/response/positionData/PositionPnL.java
@@ -10,16 +10,50 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PositionPnL extends AbstractPositionData {
-    private TradeOrderType orderType;
-    private String orderId;
-    private String closedPnl;
-    private String avgEntryPrice;
-    private String qty;
-    private String cumEntryValue;
-    private String orderPrice;
-    private String closedSize;
-    private String avgExitPrice;
-    private String execType;
-    private String fillCount;
-    private String cumExitValue;
+
+//         "symbol" -> "SEIUSDT"
+//             "leverage" -> "7.69"
+//             "autoAddMargin" -> {Integer@30534} 0
+//            "avgPrice" -> "0"
+//            "liqPrice" -> ""
+//            "riskLimitValue" -> "200000"
+//            "takeProfit" -> ""
+//            "positionValue" -> ""
+//            "isReduceOnly" -> {Boolean@35956} false
+//            "tpslMode" -> "Full"
+//            "riskId" -> {Integer@30530} 1
+//            "trailingStop" -> "0"
+//            "unrealisedPnl" -> ""
+//            "markPrice" -> "0.26981"
+//            "adlRankIndicator" -> {Integer@30534} 0
+//            "cumRealisedPnl" -> "-255.52210734"
+//            "positionMM" -> "0"
+//            "createdTime" -> "1729005375743"
+//            "positionIdx" -> {Integer@30530} 1 // 1 = Long 2 = Short
+//            "positionIM" -> "0"
+//            "seq" -> {Long@36531} 194240593182
+//            "updatedTime" -> "1740468338472"
+//            "side" -> ""
+//            "bustPrice" -> ""
+//            "positionBalance" -> "0"
+//            "leverageSysUpdatedTime" -> ""
+//            "curRealisedPnl" -> "0"
+//            "size" -> "0"
+//            "positionStatus" -> "Normal"
+//            "mmrSysUpdatedTime" -> ""
+//            "stopLoss" -> ""
+//            "tradeMode" -> {Integer@30534} 0
+//            "sessionAvgPrice" -> ""
+
+    private Integer positionIdx; // 1 = Long 2 = Short
+    private String curRealisedPnl;
+    private String unrealisedPnl;
+    private String cumRealisedPnl;
+    private String markPrice;
+    private String positionMM;
+    private String createdTime;
+    private String updatedTime;
+    private String positionBalance;
+    private String size;
+
 }
